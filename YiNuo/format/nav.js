5204988a22cbd340f3194bea4e9ff9d2b847326b
yanhua2002 <yanhua2002@msn.cn>
2016-07-28 01:05:09 +0800

    changed all ids, and nav.js

--- YiNuo/format/nav.js
+++ YiNuo/format/nav.js
@@ -7,4 +7,14 @@ $(document).ready(function(){
     $(".nav_bar .nav > ul > li").mouseleave(function(){
         $(this).removeClass("cur");
     });
+
+    /* 页内导航 */
+    jQuery('.subnav_box a').on('click',function (e){
+        var curObj=jQuery(this);
+        var curAttrValue=curObj.attr('href');
+        if(curAttrValue.indexOf("#")>-1){
+            var selTag=curAttrValue.substring(curAttrValue.indexOf("#"),curAttrValue.length);
+            jQuery("a[href='"+selTag+"']").click();
+        }
+    });
 });
